7f2bd55e40895e299e018862d11b2e568354d25d
Dnyaneshwar <dmuley123@gmail.com>
2025-07-10 18:23:38 +0530

    Created Project

---
+++ NewsPanel/NewsPanel/NewsPanelApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsPanelApp.swift
+//  NewsPanel
+//
+//  Created by Dnyaneshwar Muley on 10/07/25.
+//
+
+import SwiftUI
+
+@main
+struct NewsPanelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NewsPanel/NewsPanelTests/NewsPanelTests.swift
@@ -0,0 +1,17 @@
+//
+//  NewsPanelTests.swift
+//  NewsPanelTests
+//
+//  Created by Dnyaneshwar Muley on 10/07/25.
+//
+
+import Testing
+@testable import NewsPanel
+
+struct NewsPanelTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
